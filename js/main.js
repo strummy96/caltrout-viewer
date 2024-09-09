2ab860fb7e63da1777703e39f5421f063164c41b
strummy96 <83839689+strummy96@users.noreply.github.com>
2024-09-08 22:52:21 -0400

    plots with plotly and drains included

--- js/main.js
+++ js/main.js
@@ -1,66 +1,64 @@
+// leaflet
 let map;
+let raster_layer;
+let year = "2024";
+let dates_2023 = ['010123', '010623', '011123', '011323', '011823', '012323', '012523', '013023', '020423', '020623', '021123', '021623', '021823', '022323', '022823', '030223', '030723', '031223', '031423', '031923', '032423', '032623', '033123', '040523', '040723', '041223', '041723', '041923', '042423', '042923', '050123', '100222', '100722', '100922', '101422', '102122', '102622', '103122', '110222', '110722', '111222', '111422', '111922', '112422', '112622', '120122', '120622', '120822', '121322', '121822', '122022', '122522', '123022'];
+let dates_2024 = ['010124', '010624', '010824', '011324', '011824', '012024', '012524', '013024', '020124', '020624', '021124', '021324', '021824', '022324', '022524', '030124', '030624', '030824', '031324', '031824', '032024', '032524', '033024', '040124', '040624', '041124', '041324', '041824', '042324', '042524', '043024', '100223', '100423', '100923', '101423', '101623', '102123', '102823', '110223', '110923', '111423', '111923', '112123', '112623', '120123', '120323', '120823', '121323', '121523', '122023', '122523', '122723'];
+
 async function init_map(){
 
     open_loading_screen();
 
-    map = L.map('map-view').setView([39.006664, -121.772909], 9);
+    map = L.map('map-view', {zoomControl: false}).setView([38.65359090738684, -121.65299749322004], 12);
 
     // osm
     L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.arcgis.com/home/item.html?id=10df2279f9684e4a9f6a7f08febac2a9">ESRI</a>'
     }).addTo(map);
-    
-    // test raster
-    // L.imageOverlay(
-    //     'data/raster/NDWI_012423_mosaic_WGS84.png',
-    //     [[38.0501184391033291, -122.0439969421559852], [39.3041225336850459, -121.4857963617521222]]
-    // ).addTo(map);
-    // L.imageOverlay(
-    //     'data/raster/NDWI_012423_mosaic.png',
-    //     [[38.0501184391033291, -122.0439969421559852], [39.3041225336850459, -121.4857963617521222]]
-    // ).addTo(map);
-
-    // georaster
-    let resp = await fetch('data/raster/NDWI_012423_mosaic.tif');
-    let data = await resp.arrayBuffer();
-    let gr = await parseGeoraster(data);
-    console.log(gr);
-
-    const min = gr.mins[0];
-
-    let layer = new GeoRasterLayer({
-        georaster: gr,
-        resolution: 128,
-        pixelValuesToColorFn: function(pixelValues) {
-                  var pixelValue = pixelValues[0]; // there's just one band in this raster
-
-                  // scale to 0 - 1 used by chroma
-                  var scaledPixelValue = (pixelValue - min) / 255;
-                  let scale = chroma.scale(['black', 'white']);
-                  var color = scale(scaledPixelValue).hex();
-
-                  return color;
-        }
-    });
-    layer.addTo(map);
-    console.log(layer.getColor([100]));
 
     // add fields
     let fields_geojson = await get_geojson('data/geojson/fields.geojson');
     console.log(fields_geojson)
-    L.geoJSON(fields_geojson, {}).addTo(map);
+    let fields_layer = L.geoJSON(fields_geojson, {
+        weight: 1,
+        color: 'yellow',
+        fillColor: '#00000000',
+        onEachFeature: function(feature, layer){
+            layer.bindTooltip(feature.properties.Name, {sticky: true});
+            layer.addEventListener('mouseover', (e) => {e.target.setStyle({fillColor: '#faf43c88'})});
+            layer.addEventListener('mouseout', (e) => {e.target.setStyle({fillColor: '#00000000'})});
+            layer.on('click', (e) => {
+                console.log(e);
+                make_plot(year, e.target.feature.properties.Name); // change displayed plot
+                fields_layer.resetStyle();  // make all fields yellow again (undo previous selection)
+                e.target.setStyle({color: 'red'}); // make clicked field red
+            })
+        }
+    });
+    // fields_layer.bindTooltip({content: layer.feature.properties.Name, sticky: true});
+    // fields_layer.on('mousover', function (e) {console.log('popup'); this.openPopup()})
+    fields_layer.addTo(map);
+
+    // add dates to dropdown
+    let ul = document.querySelector("#date-dropdown-ul");
+    for(date of dates_2024){
+        let li = document.createElement('li');
+        let btn = document.createElement('button');
+        btn.classList.add('dropdown-item');
+        btn.type = "button";
+        btn.textContent = date;
+        btn.onclick = function(){
+            add_new_raster(year, this.textContent);
+        };
+        li.append(btn);
+        ul.append(li);
+    }
 
     close_loading_screen();
 }
 
 init_map();
-
-// let parse_georaster = require("georaster");
-
-// let GeoRasterLayer = require("georaster-layer-for-leaflet");
-
-// let test = new GeoRasterLayer()
 
 // unresolved
 async function get_geojson(path){
@@ -83,52 +81,100 @@ function close_loading_screen() {
     ls.style.display = "none";
 }
 
-// from an incredible person on github https://gist.github.com/gskema/2f56dc2e087894ffc756c11e6de1b5ed
-function colorGradient(fadeFraction, rgbColor1, rgbColor2, rgbColor3) {
-    var color1 = rgbColor1;
-    var color2 = rgbColor2;
-    var fade = fadeFraction;
+async function new_raster(year, date){
+    // georaster
+    let resp = await fetch('data/raster/' + year + '/sar_vv_' + date + '_mosaic.tif');
+    let data = await resp.arrayBuffer();
+    let gr = await parseGeoraster(data);
+    console.log(gr);
 
-    // Do we have 3 colors for the gradient? Need to adjust the params.
-    if (rgbColor3) {
-      fade = fade * 2;
+    const min = gr.mins[0];
+    const max = gr.maxs[0];
+    lyr = new GeoRasterLayer({
+        georaster: gr,
+        resolution: 128,
+        pixelValuesToColorFn: function(pixelValues) {
+                  var pixelValue = pixelValues[0]; // there's just one band in this raster
 
-      // Find which interval to use and adjust the fade percentage
-      if (fade >= 1) {
-        fade -= 1;
-        color1 = rgbColor2;
-        color2 = rgbColor3;
-      }
+                  // scale to 0 - 1 used by chroma
+                  var scaledPixelValue = (pixelValue) / 255;
+                  let scale = chroma.scale('YlGnBu').domain([1,0]).gamma(1.5);
+                  var color = scale(scaledPixelValue).hex();
+
+                  return color;
+        }
+    });
+
+    return lyr;
+}
+
+async function add_new_raster(year, date){
+    console.log("Adding new raster - date:", date);
+    // update date display
+    let dd = document.querySelector("#date-display");
+    dd.textContent = pretty_date(date);
+    try{
+        raster_layer.remove();
+    } catch (e) {
+        console.log(e)
+    }
+    raster_layer = await new_raster(year, date);
+    raster_layer.addTo(map);
+}
+
+function pick_date(date){
+    // remove current raster
+    raster_layer.remove()
+
+    // add new raster
+    raster_layer = L.GeoRasterLayer
+}
+
+function new_plot(year, field_name){
+    // let img = document.querySelector('#plot img');
+    // img.src = "data/png/" + year + "/conway/" + field_name + ".png"
+}
+
+function set_year(yr){
+    // button styles
+    let btn_2023 = document.querySelector("#btn-2023");
+    let btn_2024 = document.querySelector("#btn-2024");
+    if(yr=="2023"){
+        btn_2023.classList.add('btn-dark');
+        btn_2023.classList.remove('btn-light');
+        btn_2024.classList.add('btn-light');
+        btn_2024.classList.remove('btn-dark');
+    } else {
+        btn_2023.classList.add('btn-light');
+        btn_2023.classList.remove('btn-dark');
+        btn_2024.classList.add('btn-dark');
+        btn_2024.classList.remove('btn-light');
     }
 
-    var diffRed = color2.red - color1.red;
-    var diffGreen = color2.green - color1.green;
-    var diffBlue = color2.blue - color1.blue;
+    // set global variable
+    year = yr;
 
-    var gradient = {
-      red: parseInt(Math.floor(color1.red + (diffRed * fade)), 10),
-      green: parseInt(Math.floor(color1.green + (diffGreen * fade)), 10),
-      blue: parseInt(Math.floor(color1.blue + (diffBlue * fade)), 10),
-    };
-
-    return [gradient.red, gradient.green, gradient.blue];
-  }
-
-function colorGradientFromValue(value){
-    console.log("values: ", value);
-    let gradient = colorGradient(1/255, {red:0,green:0,blue:0}, {red:255,green:255,blue:255});
-    
-    console.log("color gradient: ", gradient)
-    return rgbToHex(gradient[0], gradient[1], gradient[2]);
+    // change dropdown values
+    let ul = document.querySelector("#date-dropdown-ul");
+    ul.innerHTML = ""; // clear it
+    let yr_picker = {'2023': dates_2023, '2024': dates_2024};
+    for(date of yr_picker[year]){
+        let li = document.createElement('li');
+        let btn = document.createElement('button');
+        btn.classList.add('dropdown-item');
+        btn.type = "button";
+        btn.textContent = date;
+        btn.onclick = function(){
+            add_new_raster(year, this.textContent)
+        };
+        li.append(btn);
+        ul.append(li);
+    }
 }
 
-function componentToHex(c) {
-    console.log('component to hex: ', c)
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-  
-function rgbToHex(r, g, b) {
-    console.log('rgb to hex: ', r, g, b)
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+function pretty_date(date){
+    // take a MMDDYY date and return MM/DD/YY
+    let d = date.substring(0,2) + '/' + date.substring(2,4) + '/' + date.substring(4,6);
+    console.log(date.substring(0,1))
+    return d;
 }
--- js/plots.js
+++ js/plots.js
@@ -2,7 +2,7 @@
 async function get_json(json){
     let resp = await fetch("data/json/" + json + ".json");
     let data = await resp.json();
-    console.log(data);
+    // console.log(data);
     return data;
 }
 
@@ -10,7 +10,7 @@ async function get_json(json){
 
 
 
-async function make_plot(field_name, year) {
+async function make_plot(year, field_name) {
     let data;
     // let data_2023 = await get_json("2023");
     let data_2024 = await get_json("2024");
@@ -19,29 +19,96 @@ async function make_plot(field_name, year) {
     console.log("data ", data);
 
     data = data.filter((x) => x.Name == field_name);
-    console.log("data filtered", data);
+    // console.log("data filtered", data);
 
     let date = data.map((x) => x.date);
 
     // pad with 0s
-    
+    let date_pad_zeroes = date.map((x) => {
+        if(String(x).length < 6){
+            return "0" + x;
+        } else {
+            return String(x);
+        }
+    });
+    // console.log("date_pad_zeroes:", date_pad_zeroes);
 
     // make date obj
-    let date_fmt = date.map((x) => {
+    let date_fmt = date_pad_zeroes.map((x) => {
         let s = String(x);
         return "20" + s.substring(4,6) + "-" + s.substring(0,2) + "-" + s.substring(2,4);
     });
-    console.log("date_fmt", date_fmt)
-    let date_sorted = date.sort(function(a,b){
+    // console.log("date_fmt", date_fmt)
+
+    let date_sorted = date_fmt.sort(function(a,b){
         let date_a = new Date(String(a));
         let date_b = new Date(String(b));
         if(date_a < date_b) return -1;
         else if(date_b < date_a) return 1;
         else return 0;
     });
-    console.log("dates sorted", date_sorted)
+    // console.log("dates sorted", date_sorted)
     let mean = data.map((x) => x.mean);
-    // console.log(date, mean);
 
-} make_plot("055D", "2024");
+    // drain dates
+    let drains;
+    if (year == "2023"){
+        drains = await get_json("conaway_drains_2023");
+    } else {
+        drains = await get_json("conaway_drains_2024");
+    }
+    console.log("drains:", drains);
+
+    let field_drains = drains.filter((x) => x["Field #"] == field_name);
+    console.log("field_drains:", field_drains);
+
+    // get dates of events - NEED TO BE PADDED WITH ZEROES!!!
+    let actual_floods = field_drains.map((drain) => new Date("20" + drain.actual_flood.substring(6,8), drain.actual_flood.substring(3,5), drain.actual_flood.substring(0,2)));
+    console.log(actual_floods)
+    let actual_fulls = field_drains.map((drain) => drain.actual_full);
+    let actual_drains = field_drains.map((drain) => drain.actual_drain);
+
+    // shapes for each one
+    let flood_shapes = actual_floods.map((x) => {
+        return {
+            type: 'line',
+            x0: x,
+            x1: x,
+            y0: 0,
+            y1: 100,
+            line: {
+                color: 'lightblue',
+                widht: 1
+            }
+        }
+    })
+
+    // make plotly plot
+    let plot_data = [
+        {
+            x: date_sorted,
+            y: mean,
+            type: 'scatter',
+            margin: {t: 0, b: 0, l: 0, r: 0},
+            responsive: true
+        }
+    ];
+
+    let layout = {
+        title: field_name,
+        yaxis: { range: [0, 100], title: "Mean" },
+        xaxis: { 
+            nticks: 50,
+            tickwidth: 2,
+            tickformat: "%m/%d",
+            title: "Date" 
+        },
+        shapes: flood_shapes
+    };
+
+    let config = { responsive: true };
+
+    Plotly.newPlot('plotly-div', plot_data, layout, config);
+
+} // make_plot("055D", "2024");
 
