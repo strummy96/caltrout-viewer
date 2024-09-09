a5515ebd692feaf25d27b2a08ef7971a02f42d93
strummy96 <83839689+strummy96@users.noreply.github.com>
2024-09-09 09:06:12 -0400

    update data, add conaway drain schedules to plots

--- js/plots.js
+++ js/plots.js
@@ -12,90 +12,114 @@ async function get_json(json){
 
 async function make_plot(year, field_name) {
     let data;
-    // let data_2023 = await get_json("2023");
+    let data_2023 = await get_json("2023");
     let data_2024 = await get_json("2024");
 
     if(year == "2023") {data = data_2023} else {data = data_2024}
-    console.log("data ", data);
+    // console.log("data ", data);
 
     data = data.filter((x) => x.Name == field_name);
     // console.log("data filtered", data);
 
-    let date = data.map((x) => x.date);
+    let date = data.map((x) => x.date_date);
+    // console.log(date)
 
-    // pad with 0s
-    let date_pad_zeroes = date.map((x) => {
-        if(String(x).length < 6){
-            return "0" + x;
-        } else {
-            return String(x);
-        }
-    });
-    // console.log("date_pad_zeroes:", date_pad_zeroes);
-
-    // make date obj
-    let date_fmt = date_pad_zeroes.map((x) => {
-        let s = String(x);
-        return "20" + s.substring(4,6) + "-" + s.substring(0,2) + "-" + s.substring(2,4);
-    });
-    // console.log("date_fmt", date_fmt)
-
-    let date_sorted = date_fmt.sort(function(a,b){
-        let date_a = new Date(String(a));
-        let date_b = new Date(String(b));
-        if(date_a < date_b) return -1;
-        else if(date_b < date_a) return 1;
-        else return 0;
-    });
-    // console.log("dates sorted", date_sorted)
     let mean = data.map((x) => x.mean);
 
-    // drain dates
-    let drains;
-    if (year == "2023"){
-        drains = await get_json("conaway_drains_2023");
-    } else {
-        drains = await get_json("conaway_drains_2024");
-    }
-    console.log("drains:", drains);
-
-    let field_drains = drains.filter((x) => x["Field #"] == field_name);
-    console.log("field_drains:", field_drains);
-
-    // get dates of events - NEED TO BE PADDED WITH ZEROES!!!
-    let actual_floods = field_drains.map((drain) => new Date("20" + drain.actual_flood.substring(6,8), drain.actual_flood.substring(3,5), drain.actual_flood.substring(0,2)));
-    console.log(actual_floods)
-    let actual_fulls = field_drains.map((drain) => drain.actual_full);
-    let actual_drains = field_drains.map((drain) => drain.actual_drain);
-
-    // shapes for each one
-    let flood_shapes = actual_floods.map((x) => {
-        return {
-            type: 'line',
-            x0: x,
-            x1: x,
-            y0: 0,
-            y1: 100,
-            line: {
-                color: 'lightblue',
-                widht: 1
-            }
+    // cycle dates
+    let cycles;
+    let flood_shapes = [];
+    try {
+        if (year == "2023"){
+            cycles = await get_json("conaway_cycles_2023");
+        } else {
+            cycles = await get_json("conaway_cycles_2024");
         }
-    })
+        // console.log("cycles:", cycles);
+    
+        let field_cycles = cycles.filter((x) => x["Field #"] == field_name);
+        // console.log("field_cycles:", field_cycles);
+    
+        // get dates of events - need to be padded with 0s in some cases
+        let actual_floods = get_padded_dates("actual_flood", field_cycles);
+        // console.log(actual_floods)
+        let actual_fulls = get_padded_dates("actual_full", field_cycles);
+        // console.log(actual_fulls)
+        let actual_drains = get_padded_dates("actual_drain", field_cycles);
+        // console.log(actual_drains)
+    
+        // shapes for each one
+        let floods_shapes = actual_floods.map((x) => {
+            return {
+                type: 'line',
+                showlegend: true,
+                x0: x,
+                x1: x,
+                y0: 0,
+                y1: 100,
+                line: {
+                    color: 'lightblue',
+                    width: 1
+                },
+                name: "Flood"
+            }
+        });
+    
+        let fulls_shapes = actual_fulls.map((x) => {
+            return {
+                type: 'line',
+                showlegend: true,
+                x0: x,
+                x1: x,
+                y0: 0,
+                y1: 100,
+                line: {
+                    color: 'blue',
+                    width: 1
+                },
+                name: "Full"
+            }
+        });
+    
+        let drains_shapes = actual_drains.map((x) => {
+            return {
+                type: 'line',
+                showlegend: true,
+                x0: x,
+                x1: x,
+                y0: 0,
+                y1: 100,
+                line: {
+                    color: 'red',
+                    width: 1
+                },
+                name: "Drain"
+            }
+        });
+        
+        flood_shapes = [].concat(floods_shapes, fulls_shapes, drains_shapes);
+    } catch(e) {
+        console.log("No drain dates for this year/field.")
+    }
 
     // make plotly plot
     let plot_data = [
         {
-            x: date_sorted,
+            x: date,
             y: mean,
-            type: 'scatter',
+            // type: 'scatter',
+            mode: 'lines+markers',
             margin: {t: 0, b: 0, l: 0, r: 0},
-            responsive: true
+            name: "SAR",
+            line: {
+                color: 'black'
+            }
         }
     ];
 
     let layout = {
         title: field_name,
+        showlegend: true,
         yaxis: { range: [0, 100], title: "Mean" },
         xaxis: { 
             nticks: 50,
@@ -112,3 +136,14 @@ async function make_plot(year, field_name) {
 
 } // make_plot("055D", "2024");
 
+function get_padded_dates(event, field_cycles){
+    return field_cycles.map((cycle) => {
+        let af = cycle[event];
+        let af_split = af.split("/");
+        console.log("af_split:", af_split)
+        let af_split_padded = af_split.map((x) => {if(x.length == 1){return "0" + x} else {return x}});
+        console.log("af_split_padded:", af_split_padded)
+        console.log(Number("20" + af_split_padded[2]), Number(af_split_padded[0]) - 1, Number(af_split_padded[1]))
+        return new Date(Number("20" + af_split_padded[2]), Number(af_split_padded[0]) - 1, Number(af_split_padded[1]));
+    });
+}
